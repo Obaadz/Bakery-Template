1df88b821f5f8388dac5bc767506f7c6a71923a2
Obada <obada8411@gmail.com>
2022-08-09 22:47:14 +0200

    feat: Finish header responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,24 +3,22 @@ const HEADER_LOGO = document.querySelector("header .logo");
 const MENU = document.querySelector("header .menu");
 const MENU_ICON = document.querySelector("header .menu i");
 
+// Menu List Toggle
 MENU.addEventListener("click", () => {
   MENU_ICON.classList.toggle("fa-bars");
   MENU_ICON.classList.toggle("fa-xmark");
   MENU.toggleAttribute("show");
 });
+// End Menu List
 
+// Global Cover Checker
 const coverChecker = document.querySelector(".CoverChecker");
 
 const checkInView = new IntersectionObserver(
   (event) => {
     const coverage = event[0].intersectionRatio;
-    if (coverage <= 0) {
-      HEADER.classList.add("bg-light");
-      HEADER_LOGO.src = "../assets/images/bakery-color.png";
-    } else {
-      HEADER.classList.remove("bg-light");
-      HEADER_LOGO.src = "../assets/images/bakery-light-1.png";
-    }
+    if (coverage <= 0) enableLightHeader();
+    else disableLightHeader();
   },
   {
     root: null,
@@ -28,4 +26,18 @@ const checkInView = new IntersectionObserver(
   }
 );
 
-checkInView.observe(coverChecker);
+if (coverChecker) checkInView.observe(coverChecker);
+
+function enableLightHeader() {
+  HEADER.classList.add("bg-light");
+  HEADER_LOGO.src = "./assets/images/bakery-color.png";
+}
+
+function disableLightHeader() {
+  HEADER.classList.remove("bg-light");
+
+  if (window.innerWidth <= 991)
+    HEADER_LOGO.src = "./assets/images/bakery-color.png";
+  else HEADER_LOGO.src = "./assets/images/bakery-light-1.png";
+}
+// End Cover Checker
